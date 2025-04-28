1c5a012634048ef9c4c36c84e948664d73cef1de
Catalina Olaechea <colaechea@frba.utn.edu.ar>
2025-04-28 13:52:44 -0300

    implemente función bolaEspinosa

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,10 +5,9 @@ import Text.Show.Functions ()
 doble :: Int -> Int
 doble x = x * 2
 
-type poder = (Personajes , Personajes)
 data Personajes = Unpersonaje {
      nombre :: String,
-     poderBasico :: Poder,
+     poderBasico :: Personajes -> Personajes,
      superPoder :: String, 
      superPoderActivo :: Bool,
      cantidadDeVida :: Int
@@ -16,34 +15,30 @@ data Personajes = Unpersonaje {
 --personajes:
 Espina :: Personajes
 Espina = Unpersonaje{
-    superPoder = "granada de espinas de 5 metros de radio",
+     nombre = "Espina"
+     poderBasico = bolaEspinosa,
     superPoderActivo = True,
     cantidadDeVida = 4800
 }
 
 Pamela :: Personajes
-Espina = Unpersonaje{
-    nombre = "Espina",
-    poderBasico ="lluvia de tuercas sanadoras",
-    superPoder = "torreta curativa (full soporte)",
+Pamela = Unpersonaje{
+    nombre = "Pamela",
+    poderBasico = lluviaDeTuercas,
     superPoderActivo = False,
     cantidadDeVida = 9600 
 }
 
 -- bolaEspinosa: le quita 1000 puntos de vida a quien sea su contrincante (¡no debe quedar un número negativo!)
 restarVida :: Int -> Int -> Int
-restarVida vidaPersonaje cantidadArestar = vidaPersonaje - cantidadArestar
+restarVida vidaPersonaje cantidadArestar = max 0 (vidaPersonaje - cantidadArestar)
 
-bolaEspinosa :: Info_personajes -> Info_personajes
+bolaEspinosa :: Personajes -> Personajes
 bolaEspinosa = restarVida 1000.cantidadDeVida
 
 --lluviaDeTuercas: pueden ser sanadoras o dañinas. Las primeras le suman 800 puntos de vida a su colega y las segundas le disminuyen a la mitad la vida de quien sea su contrincante. En cualquier otro caso, no le pasa nada al personaje.
-lluviaDeTuercas :: Int -> String -> Int
-lluviaDeTuercas cantidadVidaPersonaje Tipo =
-    | Tipo == "sanadoras" = (cantidadVidaPersonaje + 800)
-    | Tipo == "dañinas" = (cantidadVidaPersonaje - (cantidadVidaPersonaje/2))
 
-lluviaDeTuercas2 :: Int -> String -> Int
-lluviaDeTuercas2 cantidadVidaPersonaje "sanadoras" = (cantidadVidaPersonaje + 800)
-lluviaDeTuercas2 cantidadVidaPersonaje "dañinas" = (cantidadVidaPersonaje - (cantidadVidaPersonaje/2))
-lluviaDeTuercas2 cantidadVidaPersonaje _ = "no existe tal super poder"
+lluviaDeTuercas :: Personajes -> Personajes
+lluviaDeTuercas cantidadDeVida "sanadoras" = (cantidadDeVida + 800)
+lluviaDeTuercas cantidadDeVida "dañinas" = (cantidadDeVida - (cantidadDeVida/2))
+lluviaDeTuercas cantidadDeVida  _ = "no existe tal super poder"
